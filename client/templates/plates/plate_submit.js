8e943a97028444cb485d415c08679ee547cc12cf
Jonathan <lee.jonathanw@gmail.com>
2016-02-27 12:31:07 -0600

    Added Plate Submission Functionality

--- client/templates/plates/plate_submit.js
+++ client/templates/plates/plate_submit.js
@@ -1,13 +1,39 @@
 Template.plateSubmit.events({ 
   'submit form': function(e) {
     e.preventDefault();
-
-    var restaurant = {
-      url: $(e.target).find('[name=url]').val(), 
-      title: $(e.target).find('[name=title]').val()
+    
+    var currentRestaurantId = this._id;
+    
+    var plate = {
+      //url: $(e.target).find('[name=url]').val(), 
+      restaurantId: currentRestaurantId, 
+      name: $(e.target).find('[name=name]').val(), 
+      url: $(e.target).find('[name=url]').val(),
+      
     };
 
-    Meteor.call('restaurantInsert', restaurant, function(error, result) { 
+    Meteor.call('plateInsert', plate, function(error, result) { 
+    // display the error to the user and abort
+    if (error)
+      return alert(error.reason);
+    
+    Router.go('restaurantPage', {_id: currentRestaurantId});
+    });
+  } 
+});
+
+
+/*Template.plateSubmit.events({ 
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var plate = {
+      name: $(e.target).find('[name=name]').val(),
+      url: $(e.target).find('[name=url]').val(),
+      restaurantId: template.data._id
+    };
+
+    Meteor.call('plateInsert', plate, function(error, result) { 
     // display the error to the user and abort
     if (error)
       return alert(error.reason);
@@ -15,7 +41,7 @@ Template.plateSubmit.events({
     Router.go('restaurantPage', {_id: result._id});
     });
   } 
-});
+});*/
 
 
 
